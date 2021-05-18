fa0d45b8c8d84f70008a0e9d85a40a3b70a03cec
Georgii Vlasov <g.vlasov@haulmont.com>
2021-05-18 16:41:01 +0400

    Purchase screen correction

--- modules/web/src/com/company/vkr/web/screens/purchase/PurchaseBrowse.java
+++ modules/web/src/com/company/vkr/web/screens/purchase/PurchaseBrowse.java
@@ -1,11 +1,25 @@
 package com.company.vkr.web.screens.purchase;
 
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.UserSessionSource;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.vkr.entity.business.Purchase;
+
+import javax.inject.Inject;
 
 @UiController("vkr_Purchase.browse")
 @UiDescriptor("purchase-browse.xml")
 @LookupComponent("purchasesTable")
 @LoadDataBeforeShow
 public class PurchaseBrowse extends StandardLookup<Purchase> {
+    @Inject
+    private CollectionLoader<Purchase> purchasesDl;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        purchasesDl.setQuery("select e from vkr_Purchase e where e.customer = :customer");
+        purchasesDl.setParameter("customer", AppBeans.get(UserSessionSource.class).getUserSession().getUser());
+        purchasesDl.load();
+    }
 }
